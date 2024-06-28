bc0910586314a2cf099619e82208bf7be7e598cf
Thymangi <154999272+Thymangi@users.noreply.github.com>
2024-06-28 14:55:28 +0200

    initial commit du dossier premeir react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
 function App() {
+  const user = {
+    nom: "Doe",
+    prenom: "John",
+  };
+  const tasks = ["Réviser JavaScript", "Apprendre React", "Préparer le repas"];
+  const tasklist = [];
+  let key = 0;
+  for (let task of tasks) {
+    key++;
+    tasklist.push(<li key={key}>{task}</li>);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Bonjour {user.prenom} {user.nom}
+        </h1>
+      </header>
+      <main className="App-main">
+        <h2>Découvrons ensemble la librairie React !</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Éditez <code>src/App.js</code> et sauvegardez pour recharger.
         </p>
+        <h3>Ma TODO list</h3>
+        <ul>{tasklist}</ul>
+      </main>
+      <footer className="App-footer">
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Tutoriels React
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
